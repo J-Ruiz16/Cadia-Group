b8314b8fd832127e2704d7d75a5def5656ce23dc
THull5 <thull5@ut.utm.edu>
2025-03-07 19:48:28 -0600

    Added average/stackImage logic

--- stacker.cpp
+++ stacker.cpp
@@ -2,66 +2,109 @@
  * @file stacker.cpp
  * @author Jocelyn Ruiz
  * @date 2025-03-07
- * @brief stacker implementation file for stacker class
- * 
- * This program reads a file stores its contents into magic_number, width, height, max_color, and pixles. 
- * Vector is used for pixel data to be stored. Then this is written to output file. 
+ * @brief Implementation of the Stacker class for stacking PPM images.
+ *
+ * This program reads multiple PPM files, averages their pixel values, 
+ * and writes the final stacked image to a new PPM file.
  */
 
-#include "stacker.h"
-#include <vector>
-#include <fstream>
-#include <string>
-#include <iostream>
+ #include "stacker.h"
+ #include <vector>
+ #include <fstream>
+ #include <string>
+ #include <iostream>
+ #include <sstream>
+ 
+ Stacker::Stacker() {//Intializes default values for image contents
+     magic_number = "";
+     width = 0;
+     height = 0;
+     max_color = 255;
+     image_count = 0;
+ }
+ 
+ int Stacker::loadImage(const std::string& filename) {
+     std::ifstream fimage(filename); //Open file for reading
+     
+     if (!fimage) {
+         std::cerr << "Error in opening file " << std::endl;
+         return 1; //return 1 if file cannot be opened
+     }
+ 
+     // read the header of the file
+     std::string file_magic_number;
+     int file_width, file_height, file_max_color;
+     
+     fimage >> file_magic_number >> file_width >> file_height >> file_max_color;
+ 
+     if (magic_number.empty()) {
+         //first image sets these values
+         magic_number = file_magic_number;
+         width = file_width;
+         height = file_height;
+         max_color = file_max_color;
+         pixels.resize(width * height, {0, 0, 0}); 
+         //initialize all pixels to zero
+     } 
+ 
+     //read and accumulate pixel values
+     for (int i = 0; i < width * height; i++) {
+         int r, g, b;
+         fimage >> r >> g >> b;
+ 
+         pixels[i].red += r;
+         pixels[i].green += g;
+         pixels[i].blue += b;
+     }
+ 
+     fimage.close();
+     image_count++;
+     return 0; // return success
+ }
+ 
+ int Stacker::outImage(const std::string& outfilename) {
+     std::ofstream file(outfilename); //open file for writing
+     
+     if (!file) {
+         std::cerr << "Error " << std::endl;
+         return 1;
+     }
+ 
+     // writes the ppm header
+     file << magic_number << "\n";
 
-Stacker::Stacker(){//Initializes default values for image contents
-  magic_number = "";
-  width = 0;
-  height = 0;
-  max_color = 255;
-}
-int Stacker::loadImage(const std::string& filename){
-  std::ifstream fimage(filename);//Open file for reading
+     file << width << " " << height << "\n";
 
-  if (!fimage) {
-    std::cerr << "Error in opening file" << std::endl;
-    return 1;//return 1 if file cannot be opened
-  }
-  //read the header of the file
-  fimage >> magic_number;
+     file << max_color << "\n";
 
-  fimage >> width >>height >>max_color;
-
-  //following reads the pixel data
-  Pixel pixel;
-
-  for (int i = 0; i < width * height; i++){
-    fimage >> pixel.red >> pixel.green >> pixel.blue;
-    pixels.push_back(pixel);
-  }
-  fimage.close();
-  return 0;// return success
-
-}
-
-int Stacker::outImage(const std::string& outfilename){
-  std::ofstream file(outfilename);//open file for writing
-
-  if(!file){
-    std::cerr << "Error" << std::endl;
-    return 1;
-  }
-  //writes the ppm header
-  file << magic_number << "\n";
-
-  file << width << " " << height << "\n";
-
-  file << max_color << "\n";
-
-  //writes the pixel data to output file
-  for(int i = 0; i < width*height; i++){
-    file <<pixels[i].red << " " << pixels[i].green << " " << pixels[i].blue << "\n";
-  }
-  file.close();
-  return 0;//return success
-}
+ 
+     // write the averaged pixel data to output file
+     for (int i = 0; i < width * height; i++) {
+         file << pixels[i].red / image_count << " "
+              << pixels[i].green / image_count << " "
+              << pixels[i].blue / image_count << "\n";
+     }
+ 
+     file.close();
+     return 0; // return success
+ }
+ 
+ int Stacker::stackImage(const std::string& infilename, int numImage) {
+     std::cout << "Stacking images:" << std::endl;
+ 
+     for (int i = 1; i <= numImage; i++) {
+         std::ostringstream filename;
+         filename << infilename << "/" << infilename << "_"
+                  << (i < 10 ? "00" : "0") << i << ".ppm";
+ 
+         std::cout << "    " << filename.str() << std::endl;
+ 
+         if (loadImage(filename.str()) != 0) {
+             return 1; 
+         }
+     }
+ 
+     std::string read_filename = infilename + ".ppm";
+     return outImage(read_filename);
+ }
+ 
