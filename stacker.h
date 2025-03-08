cc9562adca76872b503b52777002633d5613c248
THull5 <thull5@ut.utm.edu>
2025-03-07 19:49:44 -0600

    added image_count and stackImage

--- stacker.h
+++ stacker.h
@@ -2,58 +2,58 @@
  * @file stacker.h
  * @author Jocelyn Ruiz
  * @date 2025-03-07
- * @brief Class definition stacker for stacking images
+ * @brief Class definition for Stacker, which stacks multiple noisy images into a clearer image.
  *
- * This class uses private members to store the values width, height, max_color, magic_number. 
- * Uses a vector with struct Pixel to store the pixel data. Methods to read and write file data
+ * This class handles reading PPM images, performing pixel averaging for denoising, 
+ * and writing the final stacked image to a new PPM file.
  */
-#include <vector>
-#include <string>
 
-#ifndef STACKER_H
-#define STACKER_H
-
-
-/**
- * reads ppm images, performs averaging for clearer image, stacks images, writing stacked image to ppm file
- *
- * @class Stacker{ stacker.h "cadia_group/stacker.h"
- * @brief class for stacking images to reduce noisy image
- *
- */
-class Stacker{
+ #ifndef STACKER_H
+ #define STACKER_H
+ 
+ #include <vector>
+ #include <string>
+ 
+ class Stacker {
  private:
-  struct Pixel{
-    int red;
-    int green;
-    int blue;
-  };
-  std::string magic_number;
-  int width;
-  int height;
-  int max_color;
-  std::vector<Pixel> pixels; //vector of pixel structs storing color information for the stacked image.
-
+     struct Pixel {
+         int red;
+         int green;
+         int blue;
+     };
+ 
+     std::string magic_number;
+     int width;
+     int height;
+     int max_color;
+     std::vector<Pixel> pixels; // Vector storing the final stacked image pixel data
+     int image_count; // Track number of images stacked
+ 
  public:
-  Stacker();
-
-/**
- * read image file data 
- *
- * @param const std::string& filename the name of the file that will be opened
- * @return int return 0 if everything goes smoothly
- * 
- */
-  int loadImage(const std::string& filename);
-
-/**
- * writes the image to ouput file 
- *
- * @param const std::string& filename the name of the file that the sample data will be written to
- * @return int return 0 if everything runs correctly
- * 
- */
-  int outImage(const std::string& outfilename);
-
-};
-#endif //STACKER_H
+     Stacker();
+ 
+     /**
+      * Reads and adds image data from a PPM file.
+      * @param filename The name of the file to open and read.
+      * @return 0 if successful, 1 on error.
+      */
+     int loadImage(const std::string& filename);
+ 
+     /**
+      * Outputs the final stacked image to a PPM file.
+      * @param outfilename The name of the output file.
+      * @return 0 if successful, 1 on error.
+      */
+     int outImage(const std::string& outfilename);
+ 
+     /**
+      * Stacks multiple images by averaging their pixel values.
+      * @param infilename The name of the images input (e.g., "orion").
+      * @param numImage The number of images to stack.
+      * @return 0 if successful, 1 on error.
+      */
+     int stackImage(const std::string& infilename, int numImage);
+ };
+ 
+ #endif // STACKER_H
+ 
